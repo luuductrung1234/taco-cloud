e51ad8b90d78fa3697586ca0c1813a928f3d2c13
Trung Luu <luuductrung1234@gmail.com>
2021-08-09 20:46:07 +0700

    input validation

--- taco-cloud/taco-cloud-web/src/main/java/com/learn/tacocloudweb/controllers/DesignTacoController.java
+++ taco-cloud/taco-cloud-web/src/main/java/com/learn/tacocloudweb/controllers/DesignTacoController.java
@@ -7,8 +7,10 @@ import com.learn.tacoclouddomain.repositories.IngredientRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -40,7 +42,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco) {
+    public String processTaco(@Valid Taco taco, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         log.info("Processing taco: " + taco);
 
         // TODO: save to database
