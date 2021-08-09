0bb5759f2647a361c44132f5505deab7d713db90
Trung Luu <luuductrung1234@gmail.com>
2021-08-09 21:26:04 +0700

    disable thymeleaf cache

--- taco-cloud/taco-cloud-web/src/main/java/com/learn/tacocloudweb/controllers/DesignTacoController.java
+++ taco-cloud/taco-cloud-web/src/main/java/com/learn/tacocloudweb/controllers/DesignTacoController.java
@@ -41,7 +41,7 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(@Valid Taco taco, Errors errors) {
+    public String processTaco(@Valid @ModelAttribute("taco") Taco taco, Errors errors) {
         if (errors.hasErrors()) {
             return "design";
         }
